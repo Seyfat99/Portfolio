66cff99c1a99917e3eced802baebd0f8d1c9154e
Seyfat Khamidov <skhamidov@rv-clt-skhamidov.redventures.net>
2018-08-15 21:35:01 -0400

    made arrow on home clickable

--- scripts.js
+++ scripts.js
@@ -1,46 +1,49 @@
 
-var hamburgerNav = document.querySelector('.hamburger-nav');
-var selectHamburger = document.querySelector('.hamburger');
+//Display hamburger menu on click
+var hamburgerNav = document.querySelector(".hamburger-nav");
+var selectHamburger = document.querySelector(".hamburger");
 
-selectHamburger.addEventListener('click', function() {
-    hamburgerNav.classList.toggle('open');
-});            
+selectHamburger.addEventListener("click", function() {
+  hamburgerNav.classList.toggle("open");
+});
 
 //Form Validation
 
 document.myForm.onsubmit = function(e) {
-    firstLastName();
-    email();
-    e.preventDefault();
+  firstLastName();
+  email();
+  e.preventDefault();
+};
+
+function firstLastName() {
+  var firstName = document.myForm.firstName.value;
+  var lastName = document.myForm.lastName.value;
+
+  if (firstName === "" || lastName === "") {
+    alert("First And Last Name Is Required");
+    return false;
+  }
+
+  if (firstName.length <= 1) {
+    alert("First Name is too short");
+    return false;
+  }
+
+  if (lastName.length <= 1) {
+    alert("Last Name is too short");
+    return false;
+  }
 }
+function email() {
+  var email = document.myForm.email.value;
+  // var emailRegex = /[a-z]{2,10}@[a-z]{2,10}.[a-z]{2,3}/i;
 
-function firstLastName () {
-
-    var firstName = document.myForm.firstName.value;
-    var lastName = document.myForm.lastName.value;
-
-    if (firstName === "" || lastName === "") {
-        alert('First And Last Name Is Required');
-        return false;
-    }
-
-    if (firstName.length < 1 || lastName.length < 1) {
-        alert('first and last name are too short');
-        return false;
-    }
-    
-}
-function email () {
-
-    var email = document.myForm.email.value;
-    // var emailRegex = /[a-z]{2,10}@[a-z]{2,10}.[a-z]{2,3}/i;
-
-    if (email === "") {
-        alert('Email Is Required');
-        return false;
-    }
-    // else if(emailRegex.test(email)) {
-    //     alert('please enter a valid email');
-    //     return false;
-    // }
+  if (email === "") {
+    alert("Email Is Required");
+    return false;
+  }
+  // else if(emailRegex.test(email)) {
+  //     alert('please enter a valid email');
+  //     return false;
+  // }
 }
